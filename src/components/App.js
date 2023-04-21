f9f35e6d32098dc9d5d52959c2e54ddc3ed38bcb
Jesse <jessemutua76@gmail.com>
2023-04-21 14:48:26 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return ( <
+        div >
+        <
+        NavBar / >
+        <
+        Home / >
+        <
+        About / >
+        <
+        /div>
+    );
 }
 
 export default App;
